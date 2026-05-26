193437e34ea33aaf20fe2f1cc733472b3373b024
Dhruv <dhruv.nautiyal420@gmail.com>
2026-05-26 14:53:05 +0200

    update and delete rows
    Added Toy Story 4 and its box office record, corrected director and year for existing movies, and removed movies released before 2005 and directed by Andrew Stanton.

--- SQL_practice/practice.sql
+++ SQL_practice/practice.sql
@@ -125,6 +125,41 @@ FROM movies
         ON movies.id = boxoffice.movie_id
 GROUP BY director;
 
+--Add the studio's new production, Toy Story 4 to the list of movies (you can use any director)
+insert into movies
+(id,title,director,year,length_minutes)
+values (15,"Toy story 4","Meenal Humnekar",2026,115)
+
+--Toy Story 4 has been released to critical acclaim! It had a rating of 8.7, and made 340 million domestically and 270 million internationally. Add the record to the BoxOffice table
+insert into boxoffice
+(movie_id,rating,domestic_sales,international_sales)
+values (15,8.7,340000000,270000000)
+
+--The director for A Bug's Life is incorrect, it was actually directed by John Lasseter
+update movies
+set director = "John Lasseter"
+where title = "A Bug's Life"
+
+--The year that Toy Story 2 was released is incorrect, it was actually released in 1999
+update movies 
+set Year = 1999
+where title = "Toy Story 2";
+
+--Both the title and director for Toy Story 8 is incorrect! The title should be "Toy Story 3" and it was directed by Lee Unkrich
+UPDATE movies
+SET title = "Toy Story 3", director = "Lee Unkrich"
+WHERE id = 11;
+
+--This database is getting too big, lets remove all movies that were released before 2005.
+delete from movies
+where year < 2005
+
+  --Andrew Stanton has also left the studio, so please remove all movies directed by him. 
+delete from movies
+where director= "Andrew Stanton";
+
+
+
 
 
 
